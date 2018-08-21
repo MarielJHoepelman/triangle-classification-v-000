b6334d78a02b24b9369f986019a3308b45af5dba
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-21 01:31:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,7 @@ class Triangle
     elsif @l1 < @l2 && @l3 < @l2 && @l1 == @l3
       raise TriangleError
     elsif @l1 > @l2 && @l2 > @l3 && @l1 > @l3
+      raise TriangleError
     end
     if @l1 == @l2 && @l2 == @l3
       :equilateral
