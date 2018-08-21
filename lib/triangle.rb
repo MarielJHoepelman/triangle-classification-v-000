4f309d175c164d20971512d584c01963c9880b2b
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-21 01:33:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,6 +26,18 @@ class Triangle
     end
   end
 
+  def is_illegal?
+    if @l1 <= 0 || @l2 <= 0 || @l3 <= 0
+      raise TriangleError
+    elsif @l1 < @l3 && @l2 < @l3 && @l1 == @l2
+      raise TriangleError
+    elsif @l1 < @l2 && @l3 < @l2 && @l1 == @l3
+      raise TriangleError
+    elsif @l1 > (@l2 + @l3)
+      raise TriangleError
+    end
+  end 
+
   class TriangleError < StandardError
   end
 end
