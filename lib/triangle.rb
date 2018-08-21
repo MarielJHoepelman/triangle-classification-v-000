c41a633e10721eb170e9d32daae96179b6287f31
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-21 01:17:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,12 +8,13 @@ class Triangle
   end
 
   def kind
+    if @l1 <= 0 || @l2 <= 0 || @l3 <= 0
     if @l1 == @l2 && @l2 == @l3
       :equilateral
     elsif @l1 == @l2 || @l2 == @l3 || @l1 == @l3
       :isosceles
     else
-      :scalene 
+      :scalene
     end
   end
 end
