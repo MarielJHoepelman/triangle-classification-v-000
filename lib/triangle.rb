46eacdd05fe6b0ebee3e91809a1fe5d2787b3f28
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-21 01:36:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,10 @@ class Triangle
   end
 
   def is_illegal?
-    (@l1 <= 0 || @l2 <= 0 || @l3 <= 0)
+    (@l1 <= 0 || @l2 <= 0 || @l3 <= 0) ||
+    (@l1 < @l3 && @l2 < @l3 && @l1 == @l2) ||
+    (@l1 < @l2 && @l3 < @l2 && @l1 == @l3) ||
+    (@l1 > (@l2 + @l3))
   end
 
   class TriangleError < StandardError
