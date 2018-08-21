25a2cb71a5063ea69a8a03f27dc209ce9bea1031
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-08-21 01:25:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   def kind
     if @l1 <= 0 || @l2 <= 0 || @l3 <= 0
       raise TriangleError
-    elsif @l1 < @l3 && @l2 < @l3
+    elsif @l1 < @l3 && @l2 < @l3 && @l1 == @l2
       raise TriangleError
     end
     if @l1 == @l2 && @l2 == @l3
